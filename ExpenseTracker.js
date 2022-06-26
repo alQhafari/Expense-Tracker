a89e804ed62047713e67efa6339d615f123077ba
Abi Al Qhafari <95538458+alQhafari@users.noreply.github.com>
2022-06-26 16:09:55 +0700

    Add files via upload

---
+++ ExpenseTracker.js
@@ -0,0 +1,50 @@
+$("#btn").click(function () {
+  let nama = $("#namaBarang").val();
+  let jumlah = $("#jumlah").val();
+  let date = $("#tanggal").val();
+  let harga = $("#harga").val();
+
+  let child = `<tr id="${nama}">
+    <td>${nama}</td>
+    <td>${jumlah}</td>
+    <td>Rp${harga}</td>
+    <td>${date}</td>
+    <td>
+      <button class="btn" onclick="deleteItem(${nama})" type="button" id="button-add">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M17.7427 8.46448L19.1569 9.87869L17.0356 12L19.157 14.1214L17.7428 15.5356L15.6214 13.4142L13.5 15.5355L12.0858 14.1213L14.2072 12L12.0859 9.87878L13.5002 8.46457L15.6214 10.5858L17.7427 8.46448Z"
+            fill="currentColor"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M8.58579 19L2.29289 12.7071C1.90237 12.3166 1.90237 11.6834 2.29289 11.2929L8.58579 5H22.5857V19H8.58579ZM9.41421 7L4.41421 12L9.41421 17H20.5857V7H9.41421Z"
+            fill="currentColor"
+          />
+        </svg>
+      </button>
+    </td>
+  </tr>`;
+  if (nama == "" || jumlah == "" || date == "" || harga == "") {
+    alert("Data Belum lengkap");
+  } else {
+    $("#table").append(child);
+  $("#namaBarang").val("");
+  $("#jumlah").val("");
+  $("#tanggal").val("");
+  $("#harga").val("");
+  }
+
+  
+});
+
+function deleteItem(item) {
+  $(item).remove();
+}
